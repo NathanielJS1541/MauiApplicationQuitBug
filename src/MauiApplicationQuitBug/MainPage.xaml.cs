7e887abb05828a3cafa9a2a116d66d90bdf84e83
Nathaniel Struselis <Nathaniel.Struselis@mail.nidec.com>
2025-02-05 19:37:24 +0000

    Added buttons to app to highlight issue.

--- src/MauiApplicationQuitBug/MainPage.xaml.cs
+++ src/MauiApplicationQuitBug/MainPage.xaml.cs
@@ -1,25 +1,91 @@
-﻿namespace MauiApplicationQuitBug
+﻿using Android.Preferences;
+
+namespace MauiApplicationQuitBug
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        #region Constants
+
+        #region Private
+
+        /// <summary>
+        /// The key to save the preference with for demonstration.
+        /// </summary>
+        private const string LastQuitTimeKey = "LastQuitTime";
+
+        #endregion
+
+        #endregion
+
+        #region Construction
 
         public MainPage()
         {
             InitializeComponent();
+
+            // When the page loads, display the previous value stored using the Preferences API.
+            // This will fall back to "N/A" to indicate no preference has been set.
+            LastQuitTime.Text = Preferences.Get(LastQuitTimeKey, "N/A");
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        #endregion
+
+        #region Methods
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Handler for the "Quit" button being clicked. This attempts to write the current time as
+        /// a string to the <see cref="Preferences"/> API, and then quits the app.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnQuitClicked(object sender, EventArgs e)
         {
-            count++;
+            // Save the current time as a string using the preferences API.
+            Preferences.Set(LastQuitTimeKey, DateTime.Now.ToString("HH:mm:ss"));
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Close the application.
+            Application.Current?.Quit();
         }
+
+        /// <summary>
+        /// Handler for the "QuitWithWorkaround" button being clicked. This attempts to write the
+        /// current time as a string to the <see cref="Preferences"/> API, manually blocks until
+        /// the value is committed using a workaround, and then quits the app.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnQuitWorkaroundClicked(object sender, EventArgs e)
+        {
+            // Save the current time as a string using the preferences API.
+            Preferences.Set(LastQuitTimeKey, DateTime.Now.ToString("HH:mm:ss"));
+
+            // HACK! Manually block until all changes are committed to disk.
+
+            // Get an ISharedPreferences instance, and open the ISharedPreferencesEditor for it.
+#pragma warning disable CA1422
+            using var sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(Android.App.Application.Context);
+#pragma warning restore CA1422
+            using var editor = sharedPreferences?.Edit();
+
+            // According to
+            // https://developer.android.com/reference/android/content/SharedPreferences.Editor#apply():
+            // > If another editor on this SharedPreferences does a regular commit() while a
+            // > apply() is still outstanding, the commit() will block until all async commits
+            // > are completed as well as the commit itself.
+            //
+            // Use this fact to block here until all outstanding applied changes are written to the
+            // disk.
+            editor?.Commit();
+
+            // Close the application.
+            Application.Current?.Quit();
+        }
+
+        #endregion
+
+        #endregion
     }
 
 }
