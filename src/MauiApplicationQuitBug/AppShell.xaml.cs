8a91aa8df2d0bdd34f1b798b5680b45293aa5cd5
Nathaniel Struselis <Nathaniel.Struselis@mail.nidec.com>
2025-02-05 16:29:40 +0000

    Added sample MAUI app.

---
+++ src/MauiApplicationQuitBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApplicationQuitBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MauiApplicationQuitBug/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApplicationQuitBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
